4e1ce4879a065291e94d637884a0341cc0058f9b
Alex Casadevall <alex@lexartlabs.com>
2022-04-01 19:33:42 -0300

    add: ejercicios rapidos

--- backend01/app.js
+++ backend01/app.js
@@ -1,5 +1,29 @@
-const AlgoBuscaNombre = require('./services/algobuscanombre.service')
-console.log( AlgoBuscaNombre.obtenerNombre('ad') )
+const express = require('express')
+const app     = express()
+const port    = 3000
 
-// const {obtenerNombre} = require('./services/algobuscanombre.service')
-// console.log( obtenerNombre('ad') )
+const AlgoBuscaNombre = require('./services/algobuscanombre.service')
+
+
+app.get('/', (req, res) => {
+    res.send({
+        respuesta: "Hola Mundo"
+    })
+})
+
+
+// CASO DE TEST
+// Si escribo: http://localhost:3000/obtener-nombre/casadevall - me deuvelve un objeto: { apellido: "Casadevall" }
+// 
+app.get('/obtener-nombre/:apellido', (req, res) => {
+    const apellido = req.params.apellido
+    const nombre   = AlgoBuscaNombre.obtenerNombre(apellido)
+    
+    res.send({
+        nombre: nombre
+    })
+})
+
+app.listen(port, (req, res) => {
+    console.log("WEBSERVER ::: ", port)
+})
